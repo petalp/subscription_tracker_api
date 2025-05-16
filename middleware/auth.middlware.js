12a103959185945935cf23b6339cd39343054f5f
petalp <peteralpha09@gmail.com>
2025-05-16 17:56:02 +0000

    created arcjet files and add some configurations to the configure folder

---
+++ middleware/auth.middlware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config/env.js';
+import  User  from '../models/user.models.js';
+
+// Middleware to check if the user is authenticated 
+const authorize = async (req, res, next) => {
+    try{
+        let token;
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+        if (!token){
+            return res.status(401).json({message : "Unauthorized no token provided"})
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        const user = await User.findById(decoded.userId);
+        if (!user){
+            return res.status(401).json({message : "Unauthorized user not found"})
+        }
+
+        req.user = user;
+        next();
+    }   catch(error){
+        return res.status(401).json({message :"Unauthorized", error:error.message})
+    }
+}
+
+
+export default authorize;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controller/user.controller.js";
+import authorize from "../middleware/auth.middlware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res)=> res.send({title:'GET all users '}));
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res)=> res.send({title :'GET user details'}));
+userRouter.get('/:id',authorize, getUser);    
 
 userRouter.post('/', (req, res)=>res.send({title:'create new user'}));
 
@@ -12,4 +14,4 @@ userRouter.put('/id', (req, res)=>res.send({title:"UPDATE user"}));
 
 userRouter.delete('/id', (req, res)=>res.send({title:'DELETE user'}));
 
-export default userRouter;
+export default userRouter; 
